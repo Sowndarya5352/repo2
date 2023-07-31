e35272a2572c7829d309d536f829ddf79c3fc4bf
Sowndarya5352 <sowndaryas.ca21@kascsathy.ac.in>
2023-07-31 09:32:03 +0000

    first commit

---
+++ three_values.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+        float a,b,c;
+        printf("Enter three values seperated by space:");
+        scanf("%f %f %f",&a,&b,&c);
+        printf("The entered values are is %f %f %f\n",a,b,c);
+        return 0;
+}
+
